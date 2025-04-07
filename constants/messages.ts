8fb7161363679a50bc5021866b23089b633bba88
aniketpaul-ui <aniketp@netbramha.com>
2025-04-07 15:36:03 +0530

    Added messages, storage key as constant

---
+++ constants/messages.ts
@@ -0,0 +1,11 @@
+export enum ErrorMessages {
+    ALL_FIELDS_REQUIRED = "All fields are required.",
+    PASSWORDS_DO_NOT_MATCH = "Passwords do not match.",
+    LOGIN_FIELDS_REQUIRED = "Both fields are required.",
+  }
+  
+export enum SuccessMessages {
+    SIGNUP_SUCCESS = "Signup successful! 🎉",
+    LOGIN_SUCCESS = "You're logged in 🎉"
+}
+  
---
+++ constants/storageKey.ts
@@ -0,0 +1,6 @@
+export enum StorageKeys {
+    IS_LOGGED_IN = "isLoggedIn",
+    USER = "user",
+    SIGNUP_USER = "signupUser",
+  }
+  
